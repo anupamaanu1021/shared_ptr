0c407affc728d68bb9d0267e4d027d5c80112546
ARI <amalapati@ariglobalsolutions.com>
2019-06-06 13:26:11 +0530

    2nd commit

--- sample.cpp
+++ sample.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <memory>
 using namespace std;
 
 int main()
 {
 	shared_ptr<int>p1(new int);
-	cout<<p1.get()<<endl;
 	cout<<p1.use_count()<<endl;
 
+	shared_ptr<int>p2(p1);
+	cout<<p2.use_count()<<endl;
 	return 0;
 }
